56a352a144a32aec7e45198daddff42aac77f98f
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-16 10:45:46 -0300

    Created page form component and added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import TaskForm from './components/TaskForm'
 import TaskListTable from './components/TaskListTable'
 
 // import { Container } from './styles';
@@ -11,7 +12,10 @@ const App: React.FC = () => {
       <div className="App">
         <NavBar />
         <div className="container" style={{ marginTop: 20 }}>
-          <TaskListTable />
+          <Switch>
+            <Route path="/" exact component={TaskListTable} />
+            <Route path="/form" component={TaskForm} />
+          </Switch>
         </div>
       </div>
     </BrowserRouter>
---
+++ src/components/TaskForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const TaskForm: React.FC = () => {
+  return (
+    <Container>
+      <h1>Cadastro da Tarefa</h1>
+    </Container>
+  )
+}
+
+export default TaskForm
